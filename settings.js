e4a050e0d67b5c04885dfcdd46f68af0d8b3ba2e
GamerGirl001 <soremekunsamuel28@gmail.com>
2026-01-04 10:30:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Adm Bot',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Chelsea Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2348147920929', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
